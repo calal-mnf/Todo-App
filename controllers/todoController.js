b59200519c9122ab370ad1f5f59f4d2460fd8614
calalmnf <calalmnf@gmail.com>
2020-11-08 00:48:09 +0400

    complete todo

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -15,8 +15,14 @@ module.exports = function (app) {
 
   app.post("/todo", urlencodedParser, function (req, res) {
     data.push(req.body);
-    res.json(data);
+    res.json({ todos: data });
   });
 
-  app.delete("/todo/:item", function (req, res) {});
+  app.delete("/todo/:item", function (req, res) {
+    data = data.filter(function (todo) {
+      return todo.item.replace(/ /g, "-") !== req.params.item;
+    });
+
+    res.json(data);
+  });
 };
